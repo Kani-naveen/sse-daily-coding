07bf847192d418ee3a414a38bab13946ea802f02
Shanmughakani <kani711998@gmail.com>
2026-06-17 14:08:05 +0530

    Two Sum

--- Day-7/src/main/java/org/example/EfficientTwoSumSolution.java
+++ Day-7/src/main/java/org/example/EfficientTwoSumSolution.java
@@ -1,4 +1,29 @@
 package org.example;
 
-public class EfficientTwoSumSolution {
+ import java.util.HashMap;
+
+class EfficientTwoSumSolution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        EfficientTwoSumSolution twoSumSolution = new EfficientTwoSumSolution();
+        int[] numbers = {2,7,11,15};
+        int target = 9;
+        int[] result = twoSumSolution.twoSum(numbers,target);
+        System.out.println(result[0]+" "+result[1]);
+    }
 }
--- Day-7/src/main/java/org/example/TwoSum.java
+++ Day-7/src/main/java/org/example/TwoSum.java
@@ -1,4 +1,25 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
+    public static void main(String[] args) {
+        int target = 9;
+        int[] nums = {1,4,5,6,4,6,7};
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i< nums.length;i++){
+            int cur = nums[i];
+            //cur + x = target
+            //x = target - cur
+            int x = target - cur;
+            if(map.containsKey(x)){
+                int[] result = {map.get(x),i};
+                System.out.println(result[0]+" "+result[1]);
+
+            }
+            map.put(cur,i);
+        }
+
+    }
 }
